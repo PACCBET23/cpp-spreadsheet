01894e821742a91666533d33d03cd7faa9da3895
PACCBET23 <iilua23@gmail.com>
2023-12-11 00:18:50 +0300

    16sprint

--- spreadsheet/FormulaAST.cpp
+++ spreadsheet/FormulaAST.cpp
@@ -72,7 +72,7 @@ public:
     virtual ~Expr() = default;
     virtual void Print(std::ostream& out) const = 0;
     virtual void DoPrintFormula(std::ostream& out, ExprPrecedence precedence) const = 0;
-    virtual double Evaluate(/*добавьте сюда нужные аргументы*/ args) const = 0;
+    virtual double Evaluate(std::function<double(Position)>& args) const = 0;
 
     // higher is tighter
     virtual ExprPrecedence GetPrecedence() const = 0;
@@ -142,9 +142,32 @@ public:
         }
     }
 
-    double Evaluate(/*добавьте нужные аргументы*/) const override {
-			// Скопируйте ваше решение из предыдущих уроков.
-    }
+    double Evaluate(std::function<double(Position)>& args) const override {
+        // Скопируйте ваше решение из предыдущих уроков.
+        switch (type_) {
+                
+            case Add:
+                return lhs_->Evaluate(args) + rhs_->Evaluate(args);
+                
+            case Subtract:
+                return lhs_->Evaluate(args) - rhs_->Evaluate(args);
+                
+            case Multiply:
+                return lhs_->Evaluate(args) * rhs_->Evaluate(args);
+            
+            case Divide:
+                
+                if (rhs_->Evaluate(args) != 0) {
+                    return lhs_->Evaluate(args) / rhs_->Evaluate(args); 
+                    
+                } else {
+                    throw FormulaError(FormulaError::Category::Div0);
+                }   
+            
+            default:
+                throw std::invalid_argument("unidentified operation type");
+        }
+    }    
 
 private:
     Type type_;
@@ -180,10 +203,21 @@ public:
         return EP_UNARY;
     }
 
-    double Evaluate(/*добавьте нужные аргументы*/ args) const override {
+    double Evaluate(std::function<double(Position)>& args) const override {
         // Скопируйте ваше решение из предыдущих уроков.
-    }
-
+        switch (type_) {
+ 
+            case UnaryPlus:
+                return operand_->Evaluate(args);
+ 
+            case UnaryMinus:
+                return -operand_->Evaluate(args); 
+ 
+            default:
+                throw std::invalid_argument("unidentified operation type");
+        }
+    }    
+    
 private:
     Type type_;
     std::unique_ptr<Expr> operand_;
@@ -211,9 +245,10 @@ public:
         return EP_ATOM;
     }
 
-    double Evaluate(/*добавьте нужные аргументы*/ args) const override {
+    double Evaluate(std::function<double(Position)>& args) const override {
         // реализуйте метод.
-    }
+        return args(*cell_);
+    }    
 
 private:
     const Position* cell_;
@@ -237,7 +272,7 @@ public:
         return EP_ATOM;
     }
 
-    double Evaluate(/*добавьте нужные аргументы*/ args) const override {
+    double Evaluate(std::function<double(Position)>& args) const override {
         return value_;
     }
 
@@ -391,8 +426,8 @@ void FormulaAST::PrintFormula(std::ostream& out) const {
     root_expr_->PrintFormula(out, ASTImpl::EP_ATOM);
 }
 
-double FormulaAST::Execute(/*добавьте нужные аргументы*/ args) const {
-    return root_expr_->Evaluate(/*добавьте нужные аргументы*/ args);
+double FormulaAST::Execute(std::function<double(Position)>& args) const {
+    return root_expr_->Evaluate(args);
 }
 
 FormulaAST::FormulaAST(std::unique_ptr<ASTImpl::Expr> root_expr, std::forward_list<Position> cells)
